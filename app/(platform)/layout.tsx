6733a3c40d8a7d81dcd5385240c0716f53970b60
JM <baldonadojm0216@gmail.com>
2023-11-28 18:51:30 +0800

    feat: implement form popover

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,12 +1,14 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import React from 'react'
+import { ClerkProvider } from '@clerk/nextjs';
+import React from 'react';
+import { Toaster } from 'sonner';
 
-const PlatformLayout = ({children}: {children: React.ReactNode}) => {
+const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
+      <Toaster />
       {children}
     </ClerkProvider>
-  )
-}
+  );
+};
 
-export default PlatformLayout
+export default PlatformLayout;
