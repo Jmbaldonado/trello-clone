20a5015caaa48579432f67859848192e71754e08
JM <baldonadojm0216@gmail.com>
2023-11-30 16:50:02 +0800

    feat: implement list options functionality with copy and deleting the list

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -1,17 +1,22 @@
 'use client';
 
-import { updateList } from '@/actions/update-list';
-import { FormInput } from '@/components/form/form-input';
-import { useAction } from '@/hooks/use-action';
 import { List } from '@prisma/client';
 import { ElementRef, useRef, useState } from 'react';
 import { toast } from 'sonner';
 import { useEventListener } from 'usehooks-ts';
 
+import { updateList } from '@/actions/update-list';
+import { FormInput } from '@/components/form/form-input';
+import { useAction } from '@/hooks/use-action';
+
+import { ListOptions } from './list-options';
+
 interface ListHeaderProps {
   data: List;
+  onAddCard: () => void;
 }
-export const ListHeader = ({ data }: ListHeaderProps) => {
+
+export const ListHeader = ({ data, onAddCard }: ListHeaderProps) => {
   const [title, setTitle] = useState(data.title);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -32,7 +37,7 @@ export const ListHeader = ({ data }: ListHeaderProps) => {
 
   const { execute } = useAction(updateList, {
     onSuccess: data => {
-      toast.success('List renamed successfully');
+      toast.success(`Renamed to "${data.title}"`);
       setTitle(data.title);
       disableEditing();
     },
@@ -45,6 +50,7 @@ export const ListHeader = ({ data }: ListHeaderProps) => {
     const title = formData.get('title') as string;
     const id = formData.get('id') as string;
     const boardId = formData.get('boardId') as string;
+
     if (title === data.title) {
       return disableEditing();
     }
@@ -69,29 +75,30 @@ export const ListHeader = ({ data }: ListHeaderProps) => {
   useEventListener('keydown', onKeyDown);
 
   return (
-    <div className='pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2'>
+    <div className='pt-2 px-2 text-sm font-semibold flex justify-between items-start- gap-x-2'>
       {isEditing ? (
-        <form className='flex-1 px-[2px]' ref={formRef} action={handleSubmit}>
+        <form ref={formRef} action={handleSubmit} className='flex-1 px-[2px]'>
           <input hidden id='id' name='id' value={data.id} />
           <input hidden id='boardId' name='boardId' value={data.boardId} />
           <FormInput
             ref={inputRef}
-            onBlur={() => {}}
+            onBlur={onBlur}
             id='title'
-            placeholder='Enter a title'
-            defaultValue='title'
+            placeholder='Enter list title..'
+            defaultValue={title}
             className='text-sm px-[7px] py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition truncate bg-transparent focus:bg-white'
           />
           <button type='submit' hidden />
         </form>
       ) : (
         <div
-          className='w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent'
           onClick={enableEditing}
+          className='w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent'
         >
           {title}
         </div>
       )}
+      <ListOptions onAddCard={onAddCard} data={data} />
     </div>
   );
 };
